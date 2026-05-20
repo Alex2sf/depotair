b01c94336d14f4df63f35fad8082133ee279af8f
Alex <alexzarodaeli8@gmail.com>
2026-05-20 11:01:53 +0700

    useer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->redirectGuestsTo(fn () => route('owner.login'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
